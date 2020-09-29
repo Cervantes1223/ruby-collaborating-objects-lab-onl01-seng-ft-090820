9199d55fb2ecb7955c552deb8b98bb828258db5b
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-29 22:04:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,15 +8,7 @@ class Song
 
   def self.new_by_filename(file_name)
     split_name = filename.split(" - ")
-    artist = file_name.split(" - ")[0]
-    new_song = self.new(song)
-    new_song.artist_name = artist
-    new_song
+    
   end
 
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end
-
-end
+  
